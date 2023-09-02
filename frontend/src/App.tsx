1bbb233ae4413c7e890bf40a1936f698c12769ed
Tian Kiat Ng <ngtiankiat97@gmail.com>
2023-09-02 14:58:06 +0800

    Dev #15: Scaffolded login, register, home page and navbar; added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Nav from './components/navbar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Error from "./pages/Error";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Nav></Nav>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Layout.tsx
@@ -0,0 +1,12 @@
+import { Outlet, Link } from "react-router-dom";
+import Nav from '../components/Nav'
+const Layout = () => {
+  return (
+    <>
+      <Nav />
+      <Outlet />
+    </>
+  )
+};
+
+export default Layout;
