62a24d15188adb4583d0e594c1f1e6f5109f5c7d
Tian Kiat Ng <ngtiankiat97@gmail.com>
2023-09-23 20:54:19 +0800

    chore: set default dashboard to customer;

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,7 +23,7 @@ function App() {
             <Route path="vendor/login" element={<LoginVendor />} />
             <Route path="register" element={<Register />} />
             {/* <Route path="vendor/register" element={<RegisterVendor />} /> */}
-            <Route path="customer/dashboard" element={<Dashboard userRole="vendor"/>} />
+            <Route path="customer/dashboard" element={<Dashboard userRole="customer"/>} />
             <Route path="*" element={<Error />} />
           </Route>
         </Routes>
