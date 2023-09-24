f0662b4b2c851b2c12a63bc3e4be4c3d2962ecbb
sebastiankzk <40329724+sebastiankzk@users.noreply.github.com>
2023-09-24 23:25:51 +0800

    Added login session (frontend) (wip)

--- frontend/src/pages/LoginCustomer.tsx
+++ frontend/src/pages/LoginCustomer.tsx
@@ -1,5 +1,9 @@
 'use client'
 
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
 import {
   Flex,
   Box,
@@ -18,53 +22,127 @@ import {
 
 export default function LoginCustomer() {
   
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  // const [userID, setUserID] = useState('');
+  // const [session, setSession] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    fetch('http://127.0.0.1:8000/login_user', {
+         method: 'POST',
+         body: JSON.stringify({
+            username: username,
+            password: password,
+         }),
+         headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+         },
+      }).then((res) => res.json())
+        .then((data) => {
+          if(!isEmpty(data)){ // Login successful
+            Cookies.remove('auth')
+            // 20 minutes cookie
+            const expirationTime = new Date(new Date().getTime() + 1200000);
+            Cookies.set('auth', JSON.stringify(data), { expires: expirationTime });
+            /* ---- To access data in cookie ----
+             JSON.parse(Cookies.get('auth'))['userID']
+             JSON.parse(Cookies.get('auth'))['roleID']
+             JSON.parse(Cookies.get('auth'))['profileName'] */
+
+            roleRedirect(data['roleID']);
+          }
+          else{ // Login fail
+            window.location.href='../login';
+          }
+     })
+     .catch((err) => {
+        console.log(err.message);
+     });
+  }
+
+  // empty object validator
+  function isEmpty(obj: any) {
+    return Object.keys(obj).length === 0;
+  }
+
+  function roleRedirect(roleID: number) {
+    if(roleID == 1){ // Admin
+      window.location.href='../admin';
+    }
+    else if(roleID == 2){ // Vendor
+      window.location.href='../vendor';
+    }
+    else if(roleID == 3){ // Customer
+      window.location.href='../customer';
+    }
+    else{ //No role, error
+      window.location.href='../login';
+    }
+  }
+
+    useEffect(() => {
+    // Check if already logged on
+    if(Cookies.get('auth')){
+      roleRedirect(JSON.parse(Cookies.get('auth'))['roleID']);
+    }
+  }, []);
+
   return (
     <Flex
       minH={'100vh'}
       align={'center'}
       justify={'center'}
       bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Sign in</Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            To start ordering your favourite foods!
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
-                <Checkbox>Remember me</Checkbox>
-                <Text color={'blue.400'}>Forgot password?</Text>
-              </Stack>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}>
-                Sign in
-              </Button>
-            </Stack>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Sign in</Heading>
+            <Text fontSize={'lg'} color={'gray.600'}>
+              To start ordering your favourite foods!
+            </Text>
           </Stack>
-        </Box>
-      </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+              <FormControl id="username">
+                <FormLabel>Username</FormLabel>
+                <Input 
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)} />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input type="password" 
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: 'column', sm: 'row' }}
+                  align={'start'}
+                  justify={'space-between'}>
+                  <Checkbox>Remember me</Checkbox>
+                  {/* <Text color={'blue.400'}>Forgot password?</Text> */}
+                </Stack>
+                <Button type='submit'
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                  Sign in
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </form>
     </Flex>
   )
 }
