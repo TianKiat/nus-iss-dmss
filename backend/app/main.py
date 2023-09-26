9eaf965cca4f1292db0af7b3ca0412dea1e4b8ce
Robert Ong <robertong43@gmail.com>
2023-09-26 20:54:02 +0800

    Update main router

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from app.route import sample_router, register_user_router, home_user_router, login_user_router
+from app.route import sample_router, register_user_router, login_user_router, dashboard_user_router
 
 app = FastAPI()
 
@@ -17,7 +17,7 @@ app.add_middleware(
 app.include_router(sample_router.router)
 app.include_router(register_user_router.router)
 app.include_router(login_user_router.router)
-app.include_router(home_user_router.router)
+app.include_router(dashboard_user_router.router)
 
 @app.get("/")
 async def root():
