0db0e1578c006e85c1334ebebb485b01709ea6a2
ngyehao1 <ngyehao@gmail.com>
2023-09-30 01:19:19 +0800

    update UT

--- backend/app/test/test_user_profile.py
+++ backend/app/test/test_user_profile.py
@@ -1,11 +1,10 @@
 import unittest
 from unittest.mock import MagicMock
+from app.helper.test_fixtures import setup_test_fixtures
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models import user, user_profile
 from app.apicontroller.user_profile_controller import UserProfileController
-import bcrypt
-
 class TestRegisterController(unittest.TestCase):
 
     def setUp(self):
@@ -17,16 +16,20 @@ class TestRegisterController(unittest.TestCase):
         # Create the tables
         user.Base.metadata.create_all(bind=self.engine)
 
+        # Set up test fixtures (populate the database)
+        setup_test_fixtures(self.session)
+
     def tearDown(self):
         # Clean up resources
         self.session.close()
 
-    def test_register_profile(self):
+    def test_user_profile(self):
         # Mocking customer data
         mock_id = '1'
         # Call the method
-        result = UserProfileController.get_user_profile(self, self.session, mock_id)
-
+        result = UserProfileController.get_user_profile(self.session, mock_id)
         # Assert that the result is as expected
-        self.assertEqual(result, {'profileName': 'Moses', 'email': 'moses@example.com', 'phone': '12345678', 'userID': 1})
-
+        self.assertEqual(result.profileName, 'Moses')
+        self.assertEqual(result.email, 'moses@example.com')
+        self.assertEqual(result.phone, '12345678')
+        self.assertEqual(result.userID, 1)
