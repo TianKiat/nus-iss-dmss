48bc178f2f2a3b0a6c3ceca76e4d2d6e3158d5a3
sebastiankzk <40329724+sebastiankzk@users.noreply.github.com>
2023-10-01 21:39:19 +0800

    Remove useNavigate

--- frontend/src/pages/LoginCustomer.tsx
+++ frontend/src/pages/LoginCustomer.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 import {
