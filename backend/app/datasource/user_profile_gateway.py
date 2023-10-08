507cb6f0a881c6e22c126877bcb53c85caf24450
Jonathan <jyck97@gmail.com>
2023-10-08 23:06:59 +0800

    clean up code

--- backend/app/datasource/user_profile_gateway.py
+++ backend/app/datasource/user_profile_gateway.py
@@ -1,9 +1,5 @@
-import mysql.connector
 from app.models.user_profile import UserProfile
 from sqlalchemy.orm import Session
-from app.common.constants import DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE
-
-sqldb = mysql.connector.connect(host = DB_HOST, user = DB_USERNAME, password = DB_PASSWORD, database = DB_DATABASE)
 
 class UserProfileGateway():
     def __init__(self):
@@ -14,3 +10,4 @@ class UserProfileGateway():
             return db.query(UserProfile).filter(UserProfile.userID == userId).first()
         except RuntimeError as exception:
             print(f"Error: {exception}")
+            
