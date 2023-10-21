ccc9fc9d62c34429850ee7bf02d40125e58b8273
Tian Kiat Ng <ngtiankiat97@gmail.com>
2023-10-21 12:31:00 +0800

    dev: moved BadgeFactory into OrderStatusBadge component

--- frontend/src/components/OrderStatusBadge.tsx
+++ frontend/src/components/OrderStatusBadge.tsx
@@ -1,20 +1,29 @@
-import { Badge } from '@chakra-ui/react'
-interface StatusBadgeProps
-{
-    type : string
+import { Badge } from "@chakra-ui/react";
+interface StatusBadgeProps {
+  type: string;
 }
-    function BadgeFactory(status : string) {
-        switch (status) {
-            case "PENDING":
-                return <Badge variant={"solid"} colorScheme='yellow'>PENDING</Badge>
-            case "DONE":
-                return <Badge variant={"solid"} colorScheme='green'>COMPLETED</Badge>
-            default:
-                return <Badge variant={"subtle"} colorScheme='gray'>NA</Badge>
-        }
-    }
 export default function OrderStatusBadge(props: StatusBadgeProps) {
-    return (
-      BadgeFactory(props.type)
-    );
+  function BadgeFactory(status: string) {
+    switch (status) {
+      case "PENDING":
+        return (
+          <Badge variant={"solid"} colorScheme="yellow">
+            PENDING
+          </Badge>
+        );
+      case "DONE":
+        return (
+          <Badge variant={"solid"} colorScheme="green">
+            COMPLETED
+          </Badge>
+        );
+      default:
+        return (
+          <Badge variant={"subtle"} colorScheme="gray">
+            NA
+          </Badge>
+        );
+    }
   }
+  return BadgeFactory(props.type);
+}
