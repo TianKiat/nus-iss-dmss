c88fa4c222b58197f13324de42b4a7b646bb5295
sebastiankzk <40329724+sebastiankzk@users.noreply.github.com>
2023-10-29 20:55:35 +0800

    Update App.tsx, useCallback

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,53 +14,38 @@ import CreateComplaint from "./pages/CreateComplaint";
 import ComplaintDashboard from "./pages/ComplaintDashboard";
 import Complaint from "./pages/Complaint";
 import Cookies from "js-cookie";
-import { useEffect, useState } from "react";
-
-interface IUserSessionData {
-  userID: string,
-  roleID: string,
-  profileName: string,
-}
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
   const [cookies, setCookies] = useState(Cookies.get("token"));
   const apiURL = process.env.VITE_API_BASE_URL;
-  
-  async function user_token(){
-    try {
-      const token = JSON.parse(Cookies.get("token"));
-    
-      if (!token) {
-        console.error('No token found in cookies.');
-        return null;
-      }
-      const response = await fetch(`${apiURL}/user_token`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(token),
-      });
 
-      if (response.status === 200) {
-        setCookies(await response.json() as IUserSessionData);
-      } else {
-        console.error('Error:', response.statusText);
+  const fetchData = useCallback(async () => {
+    try{
+      const cookie = Cookies.get("token");
+      if(cookie){
+        const token = JSON.parse(cookie);
+        const response = await fetch(`${apiURL}/user_token`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(token),
+        });
+        if (response.status === 200) {
+          setCookies(await response.json());
+        } else {
+          console.error('Error:', response.statusText);
+        }
       }
     } catch (error) {
-      console.error('Error:', error);
-      return null;
+      console.error('Error: ', error);
     }
-  }
-
+  }, [])
+  
   useEffect(() => {
-    if (Cookies.get("token")) {
-      user_token();
-    } else {
-      //unathorised or timeout
-      setCookies(null);
-    }
-  }, [Cookies]);
+    fetchData();
+  }, [])
 
   return (
     <>
