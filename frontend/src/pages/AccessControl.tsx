49f26ce01ad604edd63491522c523dba2d7e05ce
hmloke <hmloke1011@gmail.com>
2023-11-05 17:21:20 +0800

    Update AccessControl.tsx

--- frontend/src/pages/AccessControl.tsx
+++ frontend/src/pages/AccessControl.tsx
@@ -7,10 +7,15 @@ import {
     Container,
     Select,
     CheckboxGroup,
-    Checkbox
+    Checkbox,
+    Card,
+    Alert,
+    AlertIcon,
+    CardBody
 } from "@chakra-ui/react";
 
 import { useEffect, useState } from "react";
+import {Link} from 'react-router-dom';
 
 interface AccessControlProps{
     cookies: any
@@ -31,6 +36,8 @@ export default function AccessControl (props:AccessControlProps){
     const [fullAccessControlList, setFullAccessControlList] = useState({});
     const [filterRole, setFilterRole] = useState('Admin');
     const [tempAccessList, setTempAccessList] = useState<any[]>([]);
+    const [isClicked, setIsClicked] = useState(false);
+    const [status, setStatus] = useState({isSuccessful: false});
 
     useEffect(()=>{
         const fetchAccessList = async()=>{
@@ -55,6 +62,12 @@ export default function AccessControl (props:AccessControlProps){
         fetchAccessList();
     },[])
 
+    useEffect(()=>{
+        setIsClicked(false);
+        setStatus({isSuccessful: false})
+    },[filterRole])
+
+
     async function setAccessControlList(access_role_id: number){
         try{
             const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/update_access_list`, {
@@ -65,8 +78,11 @@ export default function AccessControl (props:AccessControlProps){
                 body: JSON.stringify({roleID: access_role_id, access_list: tempAccessList}),
             });
             if (response.status == 200){
-               console.log("Updated")
-
+                console.log("Updated")
+                setIsClicked(true);
+                setStatus({isSuccessful: true})
+            }else{
+                setStatus({isSuccessful: false})
             }
 
         }catch (error){
@@ -77,6 +93,7 @@ export default function AccessControl (props:AccessControlProps){
 
     function onChangeMethod(key:any, isChecked:any){
         const tempList = tempAccessList;
+        console.log(key.toString())
         if(isChecked){
             if(tempList.includes(key)!=true){
                 tempList.push(key);
@@ -84,7 +101,7 @@ export default function AccessControl (props:AccessControlProps){
             }
         }else{
             if(tempList.includes(key)){
-                const index = tempList.findIndex(key);
+                const index = tempList.indexOf(key);
                 tempList.splice(index, 1);
                 setTempAccessList(tempList);
             }
@@ -109,31 +126,49 @@ export default function AccessControl (props:AccessControlProps){
 
     return(
         <>
-            <Container>
+            <Container size={'6xl'}>
                 <Heading paddingBlock={"1rem"} fontSize={"1.5rem"} textAlign='center'>Access Control</Heading>
-                <Stack>
-                    <Select value = {filterRole} onChange={(e)=>setFilterRole(e.target.value)}>
-                        <option value="Admin">Admin</option>
-                        <option value="Vendor">Vendor</option>
-                        <option value="Customer">Customer</option>
-                    </Select>
-                    {/* <Button onClick={()=>retrieveAccessControl()}>Retrieve Access Control For Role</Button> */}
-                </Stack>
 
-                <CheckboxGroup>
+                <Card borderStyle={'outset'} marginBottom={2} marginTop={2} borderRadius={'lg'} padding={3}>
                     <Stack>
-                        {Object.keys(fullAccessControlList).map((key)=>{
-                            return(
-                                
-                                <Checkbox id={key} onChange={(e)=>onChangeMethod(key, e.target.checked)}> {fullAccessControlList[key as keyof typeof fullAccessControlList]}</Checkbox>
-                            )
-                        })}
-                        {/* <Checkbox value={key}>{full_list[key]}</Checkbox> */}
+                        <Select value = {filterRole} onChange={(e)=>setFilterRole(e.target.value)}>
+                            <option value="Admin">Admin</option>
+                            <option value="Vendor">Vendor</option>
+                            <option value="Customer">Customer</option>
+                        </Select>
+                        {/* <Button onClick={()=>retrieveAccessControl()}>Retrieve Access Control For Role</Button> */}
                     </Stack>
-                </CheckboxGroup>
-
-            
-                <Button onClick={()=>getNewControl()}>update access</Button>
+                    <CardBody>
+                    <CheckboxGroup>
+                        <Stack spacing={2}>
+                            {Object.keys(fullAccessControlList).map((key)=>{
+                                return(
+                                    
+                                    <Checkbox id={key} onChange={(e)=>onChangeMethod(key, e.target.checked)}> {fullAccessControlList[key as keyof typeof fullAccessControlList]}</Checkbox>
+                                )
+                            })}
+                            {/* <Checkbox value={key}>{full_list[key]}</Checkbox> */}
+                        </Stack>
+                    </CheckboxGroup>
+                    </CardBody>
+                    <Button bg={"blue.400"} color={"white"} onClick={()=>getNewControl()}>Update Access</Button>
+                </Card>
+                {!isClicked?
+                <div></div>:
+                status.isSuccessful?
+                    <Alert status = 'success' rounded='lg' padding='2'>
+                        <AlertIcon />
+                        Your Request has succesfully been submitted!
+                    </Alert>
+                    :
+                    <Alert status = 'error' rounded='lg' padding='2'>
+                        <AlertIcon />
+                    Your Request has not been submitted! 
+                    </Alert>
+                }
+                <Link to = {"../"}>
+                    <Button colorScheme='teal' variant={'solid'}>Back to Admin Dashboard</Button>
+                </Link>
             </Container>
         </>
     )
